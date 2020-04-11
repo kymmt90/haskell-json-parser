8ebe5d3ae043b77ed681b6d05f04dcd729b9d0e1
Kohei Yamamoto <kymmt90@gmail.com>
2020-04-11 18:13:00 +0900

    Parse an integer

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,20 @@ module Main where
 
 import Lib
 
+import Data.Char (digitToInt)
+import Text.Parsec
+import Text.Parsec.String
+
+data JsonValue = JNumber Int
+               deriving (Eq, Show)
+
+parseJson :: Parser JsonValue
+parseJson = JNumber <$> number <?> "integer"
+
+number = do
+  n <- many1 digit
+  return . read $ n
+
 main :: IO ()
-main = someFunc
+main = do
+  parseTest parseJson "123"
