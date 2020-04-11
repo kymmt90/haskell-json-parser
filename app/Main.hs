d5762599f2b7213a02a2f9ba193b4635e2aecc5f
Kohei Yamamoto <kymmt90@gmail.com>
2020-04-11 18:34:33 +0900

    Parse a null value

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ import Text.Parsec.String
 data JsonValue = JNumber Int
                | JString String
                | JBool Bool
+               | JNull
                deriving (Eq, Show)
 
 parseJson :: Parser JsonValue
@@ -16,6 +17,7 @@ parseJson = JNumber <$> (number <?> "integer")
             <|> JString <$> str
             <|> JBool True <$ boolTrue
             <|> JBool False <$ boolFalse
+            <|> JNull <$ nullValue
 
 number = do
   n <- many1 digit
@@ -27,9 +29,12 @@ boolTrue = string "true"
 
 boolFalse = string "false"
 
+nullValue = string "null"
+
 main :: IO ()
 main = do
   parseTest parseJson "123"
   parseTest parseJson "\"abc\""
   parseTest parseJson "true"
   parseTest parseJson "false"
+  parseTest parseJson "null"
