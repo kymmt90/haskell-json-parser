88bb4203ac6f02f297b50f9bccdf73dc4d3cf7b4
Kohei Yamamoto <kymmt90@gmail.com>
2020-04-11 18:26:56 +0900

    Parse a string

--- app/Main.hs
+++ app/Main.hs
@@ -7,15 +7,20 @@ import Text.Parsec
 import Text.Parsec.String
 
 data JsonValue = JNumber Int
+               | JString String
                deriving (Eq, Show)
 
 parseJson :: Parser JsonValue
-parseJson = JNumber <$> number <?> "integer"
+parseJson = JNumber <$> (number <?> "integer")
+            <|> JString <$> str
 
 number = do
   n <- many1 digit
   return . read $ n
 
+str = char '"' *> many (noneOf ['"']) <* char '"'
+
 main :: IO ()
 main = do
   parseTest parseJson "123"
+  parseTest parseJson "\"abc\""
